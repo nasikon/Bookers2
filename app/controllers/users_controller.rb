46568f3c8d49c075ddb2465b71424d600ba0c0b9
EC2 Default User <ec2-user@ip-172-31-40-60.ap-northeast-1.compute.internal>
2022-10-26 10:16:58 +0000

    finish

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @user = current_user
+    @book = Book.new
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+      
+    end 
+  end
+
+
+
+  private
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to user_path(current_user.id)
+    end
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
+  resources :users, only: [:show, :index, :edit, :update]
+
+  resources :books, only: [:edit, :index, :show, :create, :destroy, :update]
+
+  root to: "homes#top"
+
+  get 'home/about' => "homes#about", as: "about"
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
